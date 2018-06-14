d1f2348745db3096e79e0574ff188c9f131ac992
Sávio Mendes <saviomd@gmail.com>
2018-06-14 15:58:50 -0300

    Add user link component

---
+++ src/components/LinkUser.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  router-link(:to='targetRoute')
+    img(:alt='user.login_avatar', :src='user.avatar_url', width='32')
+    = ' {{ user.login }}'
+</template>
+
+<script>
+export default {
+  name: 'LinkUser',
+  props: {
+    user: { type: Object, required: true },
+  },
+  computed: {
+    targetRoute() {
+      return {
+        name: 'User',
+        params: {
+          login: this.user.login,
+        },
+      };
+    },
+  },
+};
+</script>
--- src/components/UserSearchResult.vue
+++ src/components/UserSearchResult.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
   ul
     li(v-for='item in items', :key='item.login', :value='item.login')
-      router-link(:to='{ name: "User", params: { login: item.login } }')= '{{ item.login }}'
+      link-user(:user='item')
 </template>
 
 <script>
+import LinkUser from '@/components/LinkUser';
 import store from '@/store';
 
 export default {
   name: 'UserSearchResult',
+  components: {
+    'link-user': LinkUser,
+  },
   computed: {
     items() {
       return store.state.searchUsers.items;
