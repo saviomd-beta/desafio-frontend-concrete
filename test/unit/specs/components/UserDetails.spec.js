f8000a694a46c42b79bd6a30c6bc8b0a5096f115
Sávio Mendes <saviomd@gmail.com>
2018-06-16 14:57:24 -0300

    Update user details component test

--- test/unit/specs/components/UserDetails.spec.js
+++ test/unit/specs/components/UserDetails.spec.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import UserDetails from '@/components/UserDetails';
 
 describe('UserDetails.vue', () => {
+  it('has a mounted hook', () => {
+    expect(typeof UserDetails.mounted).toBe('function')
+  })
+
   it('has correct title value', () => {
     const Constructor = Vue.extend(UserDetails);
     const vm = new Constructor({
@@ -12,4 +16,16 @@ describe('UserDetails.vue', () => {
     expect(vm.$el.querySelector('.title').textContent)
       .toEqual('saviomd');
   });
+
+  it('has correct value for avatar image alt attribute', () => {
+    const Constructor = Vue.extend(UserDetails);
+    const vm = new Constructor({
+      propsData: {
+        login: 'saviomd',
+      },
+    }).$mount();
+    expect(vm.imgAvatarAlt)
+      .toEqual('saviomd avatar');
+  });
+
 });
